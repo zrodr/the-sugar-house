34e146fb4834a468f509ed2ff900b1ed432572f7
Izaiah Rodriguez <zrodr@protonmail.com>
2021-07-12 20:49:04 -0700

    Tweaked styling for order form and added media queries for order page

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,54 @@ var itemCount = 1;
 const orderform = document.getElementById('order-form')
 const addItem = document.getElementById('add-item')
 
+const createItemField = (items) => {
+  const formGroup = document.createElement('div')
+  formGroup.setAttribute('class', 'form-group')
+
+  const itemLabel = document.createElement('label')
+  const select = document.createElement('select')
+
+  itemLabel.setAttribute('for', `item-${itemCount}`)
+  itemLabel.appendChild(document.createTextNode("Item:"))
+  select.setAttribute('name', `item-${itemCount}`)
+  select.required = true
+
+  items.forEach((item) => {
+    var option = document.createElement('option')
+    option.value = item
+    option.text = item
+
+    select.appendChild(option)
+  })
+
+  formGroup.appendChild(itemLabel)
+  formGroup.appendChild(select)
+
+  return formGroup
+}
+
+const createAmountField = () => {
+  const formGroup = document.createElement('div')
+  formGroup.setAttribute('class', 'form-group')
+
+  const amountLabel = document.createElement('label')
+  const amount = document.createElement('input')
+
+  amountLabel.setAttribute('for', `quantity-${itemCount}`)
+  amountLabel.appendChild(document.createTextNode("Amount:"))
+
+  amount.setAttribute('name', `quantity-${itemCount}`)
+  amount.setAttribute('type', 'number')
+  amount.setAttribute('min', '0')
+  amount.setAttribute('placeholder', '1000')
+  amount.required = true
+
+  formGroup.appendChild(amountLabel)
+  formGroup.appendChild(amount)
+
+  return formGroup
+}
+
 const addFormField = (e) => {
   e.preventDefault()
 
@@ -16,38 +64,14 @@ const addFormField = (e) => {
     "Lemon Pound Cake (loaf)"
   ]
 
-  const itemLabel = document.createElement('label')
-  const select = document.createElement('select')
+  const itemGroup = createItemField(items)
 
-  itemLabel.setAttribute('for', `item-${itemCount}`)
-  itemLabel.appendChild(document.createTextNode("Item:"))
-  select.setAttribute('name', `item-${itemCount}`)
-
-  const amountLabel = document.createElement('label')
-  const amount = document.createElement('input')
-
-  amountLabel.setAttribute('for', `quantity-${itemCount}`)
-  amountLabel.appendChild(document.createTextNode("Amount:"))
-  amount.setAttribute('name', `quantity-${itemCount}`)
-  amount.setAttribute('type', 'number')
-  amount.setAttribute('min', '0')
-  amount.setAttribute('placeholder', '1000')
+  const amountGroup = createAmountField()
 
   itemCount++;
 
-  items.forEach((item) => {
-    var option = document.createElement('option')
-    option.value = item
-    option.text = item
-
-    select.appendChild(option)
-  })
-
-  orderform.appendChild(itemLabel)
-  orderform.appendChild(select)
-
-  orderform.appendChild(amountLabel)
-  orderform.appendChild(amount)
+  orderform.insertBefore(itemGroup, addItem)
+  orderform.insertBefore(amountGroup, addItem)
 }
 
 if (orderform && addItem) {
