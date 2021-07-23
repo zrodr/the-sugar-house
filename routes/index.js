5f871109851e3dfabbba44b9a93e15850c91e06a
Izaiah Rodriguez <zrodr@protonmail.com>
2021-07-22 19:59:28 -0700

    Completed markup for menu section. Added common styles for buttons/scrolling. Refactored menu code into specific sections.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
+const test = [1, 2, 3]
 
 router.get('/', (req, res) => {
-  res.render('landing', {cssPath:"/css/landing.css", jsPath:""})
+  res.render('landing', {cssPath:"/css/landing.css", jsPath:"", test})
 })
+
 
 router.get('/order', (req,res) => {
   res.render('order', {cssPath:"/css/order.css", jsPath:"/js/order.js"})
